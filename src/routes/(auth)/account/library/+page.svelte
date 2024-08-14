7757ac12afc79c42021a24cff0e1f608ab10a207
sasamistreet <closedsink@gmail.com>
2024-08-14 17:28:32 +0900

    some pages

--- src/routes/(auth)/account/library/+page.svelte
+++ src/routes/(auth)/account/library/+page.svelte
@@ -7,30 +7,30 @@
     ];
 </script>
 <div class="container">
-<div class="flex justify-between my-4 p-4">
-    <h1 class="text-2xl font-bold">Library</h1>
-    <Select.Root portal={null}>
-        <Select.Trigger class="w-[180px]">
-        <Select.Value placeholder="Sort" />
-        </Select.Trigger>
-        <Select.Content>
-        <Select.Group>
-            {#each sorts as sort}
-            <Select.Item value={sort.value} label={sort.label}>{sort.label}</Select.Item>
-            {/each}
-        </Select.Group>
-        </Select.Content>
-        <Select.Input name="favoriteFruit" />
-    </Select.Root>
-</div>
-
-<div class="grid grid-cols-6 md:grid-cols-4 grid-cols-2 gap-4">
-    <div>
-        <a href="/works/1">
-            <AspectRatio ratio={1 / 1} class="bg-muted"></AspectRatio>
-        </a>
-        <p class="mt-2">title</p>
-        <p class="text-xs">by author</p>
+    <div class="flex justify-between my-4 p-4">
+        <h1 class="text-2xl font-bold">Library</h1>
+        <Select.Root portal={null}>
+            <Select.Trigger class="w-[180px]">
+            <Select.Value placeholder="Sort" />
+            </Select.Trigger>
+            <Select.Content>
+            <Select.Group>
+                {#each sorts as sort}
+                <Select.Item value={sort.value} label={sort.label}>{sort.label}</Select.Item>
+                {/each}
+            </Select.Group>
+            </Select.Content>
+            <Select.Input name="favoriteFruit" />
+        </Select.Root>
     </div>
-</div>
+
+    <div class="grid grid-cols-6 md:grid-cols-4 grid-cols-2 gap-4">
+        <div>
+            <a href="/works/1">
+                <AspectRatio ratio={1 / 1} class="bg-muted"></AspectRatio>
+            </a>
+            <p class="mt-2">title</p>
+            <p class="text-xs">by author</p>
+        </div>
+    </div>
 </div>
--- src/routes/(auth)/account/nortifications/+page.svelte
+++ src/routes/(auth)/account/nortifications/+page.svelte
@@ -1,6 +1,18 @@
-<div>
-    breadcrumb
-</div>
-<div class="flex items-center justify-between mb-4">
-    <h2 class="text-3xl font-bold tracking-tight">Nortifications</h2>
+<script lang="ts">
+
+</script>
+{#snippet nortificationItem(title:any)}
+    <div class="border-b py-4 my-4">
+        <p>{title}</p>
+    </div>
+{/snippet}
+<div class="max-w-[640px] mx-auto">
+    <div>
+        breadcrumb
+    </div>
+    <div class="flex items-center justify-between mb-4">
+        <h2 class="text-3xl font-bold tracking-tight">Nortifications</h2>
+    </div>
+    {@render nortificationItem("aaa")}
+
 </div>
