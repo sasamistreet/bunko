45d54daabe2f74f1990a0647cba6e5f1bddbf997
sasamistreet <closedsink@gmail.com>
2024-08-30 17:42:20 +0900

    aaa

--- src/routes/(auth)/account/+page.svelte
+++ src/routes/(auth)/account/+page.svelte
@@ -1,40 +1,49 @@
-<script lang="ts">
-	import { invalidate } from '$app/navigation';
-	import type { EventHandler } from 'svelte/elements';
 
+<script lang="ts">
 	import type { PageData } from './$types';
 
-	export let data: PageData;
-	$: ({ notes, supabase, user } = data);
+export let data: PageData;
+$: ({ notes, supabase, user } = data);
 
-	let handleSubmit: EventHandler<SubmitEvent, HTMLFormElement>;
-	$: handleSubmit = async (evt) => {
-		evt.preventDefault();
-		if (!evt.target) return;
-
-		const form = evt.target as HTMLFormElement;
-
-		const note = (new FormData(form).get('note') ?? '') as string;
-		if (!note) return;
-
-		const { error } = await supabase.from('notes').insert({ note });
-		if (error) console.error(error);
-
-		invalidate('supabase:db:notes');
-		form.reset();
-	};
+	import { page } from "$app/stores";
+	import {
+		Plus
+	} from "lucide-svelte";
+	import * as Avatar from "$lib/components/ui/avatar";
+	import { Button } from "$lib/components/ui/button";
+	import * as Tabs from "$lib/components/ui/tabs";
+	const userId = $page.params.id;
 </script>
-
-<h1>Private page for user: {user?.email}</h1>
-<h2>Notes</h2>
-<ul>
-	{#each notes as note}
-		<li>{note.note}</li>
-	{/each}
-</ul>
-<form on:submit={handleSubmit}>
-	<label>
-		Add a note
-		<input name="note" type="text" />
-	</label>
-</form>
+<div class="flex mx-auto gap-4 my-8">
+	<div class="w-[240px]">
+		<Avatar.Root class="mx-auto">
+			<Avatar.Image src="" alt="@shadcn" />
+			<Avatar.Fallback>CN</Avatar.Fallback>
+		</Avatar.Root>
+		<div class="text-center">
+			<p>AAA</p>
+			<p class="">@{userId}</p>
+		</div>
+		<div class="text-center">
+			<Button size="sm" variant="link" class="">
+				<Plus class="mr-2 h-4 w-4" />
+				Follow
+			</Button>
+		</div>
+		<p>bbbbbbbbbbbbbb</p>
+	</div>
+	<div class="glow">
+		<Tabs.Root value="works" class="space-y-4">
+			<Tabs.List>
+				<Tabs.Trigger value="works">Works</Tabs.Trigger>
+				<Tabs.Trigger value="gallery">Gallery</Tabs.Trigger>
+			</Tabs.List>
+			<Tabs.Content value="works" class="space-y-4">
+				
+			</Tabs.Content>
+			<Tabs.Content value="gallery" class="space-y-4">
+				
+			</Tabs.Content>
+		</Tabs.Root>
+	</div>
+</div>
--- src/routes/(public)/users/[id]/+page.svelte
+++ src/routes/(public)/users/[id]/+page.svelte
@@ -1,44 +1,45 @@
 <script lang="ts">
+	import { page } from "$app/stores";
 	import {
-		Activity,
-		CreditCard,
-		DollarSign,
-		Download,
-		Users
+		Plus
 	} from "lucide-svelte";
+	import * as Avatar from "$lib/components/ui/avatar";
 	import { Button } from "$lib/components/ui/button";
 	import * as Card from "$lib/components/ui/card";
 	import * as Tabs from "$lib/components/ui/tabs";
-
+	import { AspectRatio } from "$lib/components/ui/aspect-ratio";
+	const userId = $page.params.id;
 </script>
-<div class="grid grid-cols-6 gap-4">
-	<div>a</div>
-	<div class="col-span-4 hidden flex-col md:flex">
-		<div class="flex-1 space-y-4 p-8 pt-6">
-			<div class="flex items-center justify-between space-y-2">
-				<h2 class="text-3xl font-bold tracking-tight">Dashboard</h2>
-				<div class="flex items-center space-x-2">
-					<Button size="sm">
-						<Download class="mr-2 h-4 w-4" />
-						Download
-					</Button>
-				</div>
-			</div>
-			<Tabs.Root value="overview" class="space-y-4">
-				<Tabs.List>
-					<Tabs.Trigger value="overview">Outlines</Tabs.Trigger>
-					<Tabs.Trigger value="analytics" disabled>Articles</Tabs.Trigger
-					>
-					<Tabs.Trigger value="reports" disabled>Gallery</Tabs.Trigger>
-					<Tabs.Trigger value="notifications" disabled
-						>Works</Tabs.Trigger
-					>
-				</Tabs.List>
-				<Tabs.Content value="overview" class="space-y-4">
-					
-				</Tabs.Content>
-			</Tabs.Root>
+<div class="w-[640px] flex mx-auto gap-4 my-8">
+	<div class="w-1/3">
+		<Avatar.Root class="mx-auto">
+			<Avatar.Image src="" alt="@shadcn" />
+			<Avatar.Fallback>CN</Avatar.Fallback>
+		</Avatar.Root>
+		<div class="text-center">
+			<p>AAA</p>
+			<p class="">@{userId}</p>
 		</div>
+		<div class="text-center">
+			<Button size="sm" variant="link" class="">
+				<Plus class="mr-2 h-4 w-4" />
+				Follow
+			</Button>
+		</div>
+		<p>bbbbbbbbbbbbbb</p>
 	</div>
-	<div></div>
+	<div class="w-2/3">
+		<Tabs.Root value="works" class="space-y-4">
+			<Tabs.List>
+				<Tabs.Trigger value="works">Works</Tabs.Trigger>
+				<Tabs.Trigger value="gallery">Gallery</Tabs.Trigger>
+			</Tabs.List>
+			<Tabs.Content value="works" class="space-y-4">
+				
+			</Tabs.Content>
+			<Tabs.Content value="gallery" class="space-y-4">
+				
+			</Tabs.Content>
+		</Tabs.Root>
+	</div>
 </div>
